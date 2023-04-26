70b3b4a0d79edc835adff00a1e20fe2d06137f04
Rafael Assad <rafael.assad1010@gmail.com>
2023-04-25 23:48:48 -0300

    Completed the chalange

--- news/views.py
+++ news/views.py
@@ -1,10 +1,22 @@
+import requests
+import environ
 from rest_framework import status
 from rest_framework.response import Response
 from rest_framework.decorators import api_view, renderer_classes
 from rest_framework.renderers import JSONRenderer
 
+BASE_DIR = environ.Path(__file__) - 2
+
+env = environ.FileAwareEnv()
+env.read_env(BASE_DIR('.env'), overwrite=True)
+
 @api_view(['GET'])
 @renderer_classes([JSONRenderer])
 def listNews(request):
+    API_KEY = env('NEWS_API_KEY')
 
-    return Response({'message': 'ta funfando'}, status=status.HTTP_200_OK)
+    news_api_url = f"https://newsapi.org/v2/top-headlines?country=br&apiKey={API_KEY}"
+
+    news = requests.get(news_api_url)
+
+    return Response(news.json(), status=status.HTTP_200_OK)
