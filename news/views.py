e556bcb4f9a6b70303fba3bdfea1ee6608049bf4
Rafael Assad <rafael.assad1010@gmail.com>
2023-04-26 00:07:14 -0300

    Finished a missing requirement and add initial README

--- news/views.py
+++ news/views.py
@@ -10,6 +10,21 @@ BASE_DIR = environ.Path(__file__) - 2
 env = environ.FileAwareEnv()
 env.read_env(BASE_DIR('.env'), overwrite=True)
 
+def get_articles_main_info(articles_list):
+    filtered_articles = []
+
+    for article in articles_list:
+        new_article = {
+            "author": article['author'],
+            "title": article['title'],
+            "description": article['description'],
+        }
+        filtered_articles.append(new_article)
+    
+    return filtered_articles
+
+    pass
+
 @api_view(['GET'])
 @renderer_classes([JSONRenderer])
 def listNews(request):
@@ -17,6 +32,8 @@ def listNews(request):
 
     news_api_url = f"https://newsapi.org/v2/top-headlines?country=br&apiKey={API_KEY}"
 
-    news = requests.get(news_api_url)
+    news = requests.get(news_api_url).json()
 
-    return Response(news.json(), status=status.HTTP_200_OK)
+    news_filtered = get_articles_main_info(news['articles'])
+
+    return Response(news_filtered, status=status.HTTP_200_OK)
